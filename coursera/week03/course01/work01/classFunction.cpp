83a0b0335a0f4429f9e3aabde58c44acf3e2c7ff
Shuo <1515506566@qq.com>
2021-02-03 12:59:26 +0800

    finish class function

---
+++ coursera/week03/course01/work01/classFunction.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+
+using namespace std;
+
+class Animal
+{
+    private:
+        string name;
+        int age;
+    
+    public:
+        inline void spark();
+        inline void spark(string voice);
+
+};
+
+inline void Animal::spark()
+{
+    cout << "wolf, wolf!" << endl;
+}
+
+inline void Animal::spark(string voice)
+{
+    cout << voice << endl;
+}
+
+int main()
+{
+    Animal doggy;
+    doggy.spark("catalina");
+}
